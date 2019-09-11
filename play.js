f2ce30a20a4b1a4120d7ad09f517a8e7f04f26bc
sepuckett86 <sepuckett86@gmail.com>
2019-09-11 15:17:42 -0700

    try newsapi npm package

---
+++ play.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWSAPI_KEY);
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
