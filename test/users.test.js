9c69ddcb9a0731efe97faf394b1b950de58868b3
sepuckett86 <sepuckett86@gmail.com>
2019-09-12 14:58:17 -0700

    can retrieve a logged in user

--- test/users.test.js
+++ test/users.test.js
@@ -1,4 +1,5 @@
 const { getUsers } = require('./data-helpers');
+const User = require('../lib/models/User');
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -35,8 +36,26 @@ describe('user routes', () => {
       });
   });
 
-  // it('can get user by id', () => {
-  //   return request(app)
-  //     .
-  // })
+  it('can get logged in user', async() => {
+    await User.create({ 
+      name: 'User',
+      avatar: 'urlhere', 
+      authid: 'fake-user|12345' 
+    });
+
+    return request(app)
+      .get('/api/v1/users/logged-in-user')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'User',
+          avatar: 'urlhere',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          authid: 'fake-user|12345',
+          tags: []
+        });
+      });
+  });
+  //add patch test and route for tags
 });
