5273ea1082274c5303a0b7b1a90b1413c457b4d8
sepuckett86 <sepuckett86@gmail.com>
2019-09-13 13:26:15 -0700

    add user routes for add and delete tag

--- test/users.test.js
+++ test/users.test.js
@@ -1,4 +1,4 @@
-const { getUsers } = require('./data-helpers');
+const { getUsers, getTags } = require('./data-helpers');
 const User = require('../lib/models/User');
 
 const request = require('supertest');
@@ -57,5 +57,55 @@ describe('user routes', () => {
         });
       });
   });
-  //add patch test and route for tags
+
+  it('can add a tag to tag array', async() => {
+    await User.create({ 
+      name: 'User',
+      avatar: 'urlhere', 
+      authid: 'fake-user|12345' 
+    });
+
+    const tag = getTags()[0];
+
+    return request(app)
+      .patch('/api/v1/users/add-tag')
+      .send({ tag: tag._id })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'User',
+          avatar: 'urlhere',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          authid: 'fake-user|12345',
+          tags: [tag._id]
+        });
+      });
+  });
+
+  it('can delete a tag from tag array', async() => {
+    const tag = getTags()[0];
+
+    await User.create({ 
+      name: 'User',
+      avatar: 'urlhere', 
+      authid: 'fake-user|12345',
+      tags: [tag._id]
+    });
+
+    return request(app)
+      .patch('/api/v1/users/delete-tag')
+      .send({ tag: tag._id })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'User',
+          avatar: 'urlhere',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          authid: 'fake-user|12345',
+          tags: []
+        });
+      });
+  });
 });
